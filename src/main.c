f3b401176b1cb1aa4c7b5b063fa51a08f9df2174
Alex <alex.m.diehl1@gmail.com>
2026-09-05 13:47:37 -0500

    glfw window

--- src/main.c
+++ src/main.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <vulkan/vulkan.h>
+
+
 int main (int argc, char **argv)
 {
-	printf ("Hello, World!\n");
+	if (!glfwInit()) { goto EXIT; }
+
+	glfwWindowHint (GLFW_CLIENT_API, GLFW_NO_API);
+	GLFWwindow *window = glfwCreateWindow(800, 600, "Hello, Vulkan", NULL, NULL);
+	if (window == NULL) { goto EXIT; }
+
+
+	for ( ;; )
+	{
+		if (glfwWindowShouldClose(window)) { break; }
+		glfwPollEvents ();
+	}
+
+
+EXIT:
+	if (window != NULL) { glfwDestroyWindow(window); }
+
+	glfwTerminate ();
 
 	return EXIT_SUCCESS;
 }
